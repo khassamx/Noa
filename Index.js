3aa301ee5da3869b4645b9369ffc6c53342822d7
khassamx <khasamxx@gmail.com>
2025-09-08 17:23:34 -0300

    Actualizar Index.js

--- Index.js
+++ Index.js
@@ -12,7 +12,7 @@ import fs from 'fs';
 const SESSION_PATH = "auth_info";
 const LOG_FILE = "./logs.txt";
 // Este es el número del dueño del bot, sin el @s.whatsapp.net
-global.owner = ["595986114722"]; 
+global.owner = ["393939393939"]; 
 
 // --- Sistema de Logs ---
 function log(message) {
@@ -44,8 +44,7 @@ async function startBot() {
         if (connection === "close") {
             const shouldReconnect = lastDisconnect.error?.output?.statusCode !== DisconnectReason.loggedOut;
             if (shouldReconnect) {
-                log(chalk.red("Conexión cerrada. Reconectando..."));
-                startBot();
+                log(chalk.red("Conexión cerrada. El bot se ha desconectado. Por favor, reinícialo manualmente."));
             } else {
                 log(chalk.red("Conexión cerrada. Sesión cerrada. Por favor, reinicia el bot para un nuevo QR."));
             }
